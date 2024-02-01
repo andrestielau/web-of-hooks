0be0242c5ccec574ede39f58adda47eef1a6657d
Daniel Yan <daniel.yan@anchorlabs.com>
2024-02-01 14:59:40 +0000

    api endpoint to list event-types and created onboarding.approved and depositattribution.pending event types

--- webhooks/adapt/http/handle/event-types.go
+++ webhooks/adapt/http/handle/event-types.go
@@ -5,6 +5,8 @@ import (
 	"woh/package/utils/media"
 	webhooksv1 "woh/webhooks/adapt/http/v1"
 	"woh/webhooks/render/page"
+
+	"github.com/samber/lo"
 )
 
 // ListChannels implements webhooksv1.ServerInterface.
@@ -15,6 +17,9 @@ func (h *Handler) ListChannels(w http.ResponseWriter, r *http.Request, params we
 // ListEventTypes implements webhooksv1.ServerInterface.
 func (h *Handler) ListEventTypes(w http.ResponseWriter, r *http.Request, params webhooksv1.ListEventTypesParams) {
 	//	h.Repo.CreateEventTypes(r.Context(), []queries.NewEventType{{"test-" + uuid.NewString()}}) uncomment to test types
+	if params.Limit == nil {
+		params.Limit = lo.ToPtr(LIMIT_DEFAULT)
+	}
 	eventTypes, err := h.Repo.ListEventTypes(r.Context(), h.Convert.EventTypeQuery(params))
 	if media.ShouldRender(r) {
 		page.EventTypes(page.EventTypesViewModel{
